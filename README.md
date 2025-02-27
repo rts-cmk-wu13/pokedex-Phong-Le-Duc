5c25467baffa7090553d6b0ada882f6f926ee2df
Phong <phongleduc@gmail.com>
2025-02-27 12:53:40 +0100

    netlify link added

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+link til NETLIFY:
+incandescent-malasada-168e07.netlify.app
+
+
+
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/GpfmwDls)
 **Dynamisk web**
 
--- home_layout.js
+++ home_layout.js
@@ -1,4 +1,29 @@
 
+
+let divElm = document.createElement("div")
+divElm.id = "root"
+
+divElm.innerHTML = `
+<header>
+
+    <img class="poke_ball" src="img/pokeball.png" alt="">
+<span class="brand ">Pokédex</span> <br>
+<form action="detail.html"></form>
+<input class="poke_search" type="search" placeholder="Search Pokémon" name="name" id="name" />
+
+</header>
+<main></main>
+<footer>created 2025</footer>
+`
+
+document.querySelector("body").append(divElm)
+
+
+
+
+
+
+
 // ----------------- BEDRE UDGAVE---------------
 
 /**
